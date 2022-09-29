21098e02ae58a99e1619568ab1ab226ee5c4a9ec
Alejandra Ledezma <101372648+AlejandraLedezma@users.noreply.github.com>
2022-09-28 23:36:32 -0500

    Add files via upload

---
+++ Tarea6_LMA.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+#define TAM 10
+
+void PatronA(int arreglo[TAM][TAM]);
+void PatronB(int arreglo[TAM][TAM]);
+void PatronE(int arreglo[TAM][TAM]);
+
+
+int main(){
+    int arreglo[TAM][TAM];
+
+    //PatronA(arreglo);
+    //PatronB(arreglo);
+    PatronE(arreglo);
+
+
+}
+
+
+//funcion para el primer patron "A"
+void PatronA(int arreglo[TAM][TAM]){
+
+    for(int r=0; r<TAM; r++){ //renglones
+        for(int c=0; c<TAM; c++){ //columnas
+            if(r==0 || r==TAM-1 || c==0 || c==TAM-1){
+                printf("1 ");
+            }else{
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+
+//funcion para el segundo patron "b"
+void PatronB(int arreglo[TAM][TAM]){
+    
+        for(int r=0; r<TAM; r++){ //renglones
+            for(int c=0; c<TAM; c++){ //columnas
+                if(r==c || c+r==TAM-1){
+                    printf("1 ");
+                }else{
+                    printf("0 ");
+                }
+            }
+        printf("\n");
+    }
+}
+
+
+
+//funcion para el segundo patron "e"
+void PatronE(int arreglo[TAM][TAM]){
+    
+        for(int r=0; r<TAM; r++){ //renglones
+            for(int c=0; c<TAM; c++){ //columnas
+             if(c+2 || r+2){
+                printf("1 ");
+             }else{
+                printf("0 ");
+             }
+            }
+        printf("\n");
+    }
+}
