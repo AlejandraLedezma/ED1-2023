cd55e53f50e0a2516b26231f0940ef464b8278de
Alejandra Ledezma <101372648+AlejandraLedezma@users.noreply.github.com>
2022-09-30 16:48:54 -0500

    Add files via upload
    C

--- CaractTa.c
+++ CaractTa.c
@@ -3,8 +3,8 @@
 #define TAM 100
 
 void PideCade(char frase[TAM]);
-void PideLetra(char letra);
-void VecesLetra(int cont, char letra, char frase[TAM]);
+void PideLetra(char *letra);
+void VecesLetra(int *cont, char letra, char frase[TAM]);
 
 int main(){
 
@@ -12,30 +12,31 @@ int main(){
     char letra;
     int cont=0;
 
-    PideCade(frase[TAM]);
-    PideLetra(letra);
-    VecesLetra(cont, letra,frase[TAM]);
-    
-    printf("La letra aparece %d veces",cont);
+    PideCade(frase);
+    PideLetra(&letra);
+    VecesLetra(&cont, letra, frase);
+
+    printf("La letra aparece %d veces", cont);
 
     return 0;
 }
 
 void PideCade(char frase[TAM]){
     printf("Dame la oracion: ");
-    scanf("\n%[^\n]",frase);
+    scanf("%[^\n]s", frase);
 }
 
-void PideLetra(char letra){
+void PideLetra(char *letra){
     printf("Dame la letra que quieres saber que se repite: ");
-    scanf("%s",letra);
+    getchar();
+    scanf("%c", &*letra);
 }
 
-void VecesLetra(int cont, char letra, char frase[TAM]){
+void VecesLetra(int *cont, char letra, char frase[TAM]){
 
-    for(int i=0; i<=TAM-1; i++){
-        if(frase[i]==letra){
-           cont=cont+1;
+    for(int i=0; i<TAM; i++){
+        if(frase[i] == letra){
+           *cont = *cont + 1;
         }
     }
 
